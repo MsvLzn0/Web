a16d9f125aaee49269f0590e9562d07e1b07965e
Petru <trifan.petru@iis.utm.md>
2022-04-03 23:49:24 +0300

    Lab3

--- web/src/Layouts/LayoutCustom.tsx
+++ web/src/Layouts/LayoutCustom.tsx
@@ -1,16 +1,52 @@
-import { Layout } from 'antd'
+import { Layout, Menu, Breadcrumb } from "antd";
+import { ContentLayout } from './ContentLayout';
+import './LayoutC.css';
+import { Forms } from '../Form/Forms';
+
+const { Header, Content, Footer } = Layout;
+
+const Informations = [
+    {
+        id: 1,
+        title: "This is title number 1",
+        description: "This is description"
+    },
+    {
+        id: 2,
+        title: "This is title number 2",
+        description: "This is description"
+    },
+    {
+        id: 3,
+        title: "This is title number 3",
+        description: "This is description"
+    },
+    {
+        id: 4,
+        title: "This is title number 4",
+        description: "This is description"
+    },
+    {
+        id: 5,
+        title: "This is title number 5",
+        description: "This is description"
+    },
 
 
-const Layout = () => {
+]
+
+
+export const LayoutCustom = () =>{
     return(
-        <div>
-            <Layout>
-                <Header className="header">
+        <>
+            <Layout className="layout">
+                <Header>
                     <div className="logo" />
                     <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                        <Menu.Item key="1">nav 1</Menu.Item>
-                        <Menu.Item key="2">nav 2</Menu.Item>
-                        <Menu.Item key="3">nav 3</Menu.Item>
+                        {new Array(4).fill(null).map((_, index) => {
+                            const key = index + 1;
+                            return <Menu.Item key={key}>{`I am bar ${key}`}</Menu.Item>;
+                        })}
                     </Menu>
                 </Header>
                 <Content style={{ padding: '0 50px' }}>
@@ -18,42 +54,22 @@ const Layout = () => {
                         <Breadcrumb.Item>Home</Breadcrumb.Item>
                         <Breadcrumb.Item>List</Breadcrumb.Item>
                         <Breadcrumb.Item>App</Breadcrumb.Item>
+                        <Breadcrumb.Item>ThisNode</Breadcrumb.Item>
                     </Breadcrumb>
-                    <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
-                        <Sider className="site-layout-background" width={200}>
-                            <Menu
-                                mode="inline"
-                                defaultSelectedKeys={['1']}
-                                defaultOpenKeys={['sub1']}
-                                style={{ height: '100%' }}
-                            >
-                                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                                    <Menu.Item key="1">option1</Menu.Item>
-                                    <Menu.Item key="2">option2</Menu.Item>
-                                    <Menu.Item key="3">option3</Menu.Item>
-                                    <Menu.Item key="4">option4</Menu.Item>
-                                </SubMenu>
-                                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                                    <Menu.Item key="5">option5</Menu.Item>
-                                    <Menu.Item key="6">option6</Menu.Item>
-                                    <Menu.Item key="7">option7</Menu.Item>
-                                    <Menu.Item key="8">option8</Menu.Item>
-                                </SubMenu>
-                                <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                                    <Menu.Item key="9">option9</Menu.Item>
-                                    <Menu.Item key="10">option10</Menu.Item>
-                                    <Menu.Item key="11">option11</Menu.Item>
-                                    <Menu.Item key="12">option12</Menu.Item>
-                                </SubMenu>
-                            </Menu>
-                        </Sider>
-                        <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
-                    </Layout>
+
+                    <div className="site-layout-content">
+                        {   Informations.map((el, index) =>{
+                            return(
+                                <ContentLayout key={el.id} title={el.title} description={el.description} />
+                            )
+                        })
+                        }
+
+                    </div>
+                    <Forms></Forms>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-            </Layout>
-        </div>
+            </Layout>,
+        </>
     )
 }
 
-export default Layout
